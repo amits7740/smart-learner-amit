faef8d74490bec7d0fd34903a9ed056403df02ec
amits7740 <30660803+amits7740@users.noreply.github.com>
2020-01-19 21:49:19 +0530

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -4,7 +4,11 @@ import './index.css';
 import Home from './components/home/home';
 import Product from './components/product/product'
 import Tshirt from './components/tshirt/tshirt'
-
+import P from './components/p/p'
+import T from './components/t/t'
+import PS from './components/ps/ps'
+import Server from './components/ps/server';
+import Cart from './components/cart/cart'
 import * as serviceWorker from './serviceWorker';
 import { Router, Route, browserHistory, IndexRoute } from "react-router";
 import 'bootstrap/dist/css/bootstrap.min.css'
@@ -17,7 +21,11 @@ ReactDOM.render(
         <Route path="product" component={Product} />
         {/* <Route path="search" component={Search} /> */}
         <Route path="tshirt" component={Tshirt} />
-        
+        <Route path="p" component={P} />
+        <Route path="t" component={T} />
+        <Route path="ps" component={PS} />
+        <Route path="server" component={Server} />
+        <Route path="cart" component={Cart} />
     </Router>, 
     
     document.getElementById('root'));
